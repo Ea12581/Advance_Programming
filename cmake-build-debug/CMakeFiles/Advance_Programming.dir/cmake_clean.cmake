fb4a41e81c361691cc550600f7e4a99509648a8d
shmini31 <shmini31@gmail.com>
2022-11-13 12:53:30 +0200

    Hello in c++

---
+++ cmake-build-debug/CMakeFiles/Advance_Programming.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Advance_Programming.exe"
+  "Advance_Programming.exe.manifest"
+  "Advance_Programming.pdb"
+  "CMakeFiles/Advance_Programming.dir/main.cpp.obj"
+  "libAdvance_Programming.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Advance_Programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
